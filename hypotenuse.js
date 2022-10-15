6b33d24d0727dea31e4df537d7a3f4aee861d0bf
Nikhil5705 <vermanikhil5705@gmail.com>
2022-10-15 22:04:04 +0530

    initial commit

--- hypotenuse.js
+++ hypotenuse.js
@@ -1,17 +1,26 @@
-const sides = document.querySelectorAll(".side-length");
-const hypotenuseLengthBtn = document.querySelector("#hypotenuse-length-btn");
-const output = document.querySelector("#output");
+var sides = document.querySelectorAll(".side-length");
+var hypotenuseLengthBtn = document.querySelector("#hypotenuse-length-btn");
+var output = document.querySelector("#output");
 
 function getSquares(a, b){
-    const sumOfSquares = a*a + b*b;
+    var sumOfSquares = a*a + b*b;
     return sumOfSquares;
 }
 
 function calculateHypotenuseLength(){
-const sumOfSquares = getSquares(Number(sides[0].value), Number(sides[1].value));
-const lengthofhypotenuse = Math.sqrt(sumOfSquares);
-output.innerText = "Hypotenuse Length is " + lengthofhypotenuse;
-
+    var sideOne = sides[0].value;
+    var sideTwo = sides[1].value;
+    if(sideOne == '' || sideTwo == ''){
+    output.innerText = "Please fill all fields";
+    }
+    else if(Number(sideOne) <= 0 || Number(sideTwo) <= 0){
+    output.innerText = "Entered Side must be greater than 0";
+    }
+    else{
+var sumOfSquares = getSquares(Number(sides[0].value), Number(sides[1].value));
+var lengthofhypotenuse = Math.sqrt(sumOfSquares);
+output.innerText = "Hypotenuse Length is " + lengthofhypotenuse.toFixed(2);
+    }
 }
 
 hypotenuseLengthBtn.addEventListener("click", calculateHypotenuseLength);
